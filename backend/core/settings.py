dd34ba241430644f66211f726adfb4a25dd8e4bf
acarbonilla <carbonillaalfredo@gmail.com>
2026-01-08 21:02:42 +0800

    Production_Static_Root_Configuration

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -155,6 +155,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.getenv("STATIC_ROOT")
+if not STATIC_ROOT:
+    raise RuntimeError(
+        "STATIC_ROOT must be set via environment variable for production collectstatic"
+    )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
